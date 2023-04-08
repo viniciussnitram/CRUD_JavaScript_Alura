3dafba3f532032fda54d3d965ec15afdb4d18655
Vinicius Martins <vinicius.martins.vsm@outlook.com>
2023-04-08 12:30:52 -0300

    Now we can edit the clients

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,10 +1,8 @@
 const listaClientes = () => {
-    
     return fetch(`http://localhost:3000/profile`)
     .then(resposta => {
         return resposta.json()
-    })   
-      
+    })
 }
 
 const criaCliente = (nome, email) => {
@@ -29,8 +27,33 @@ const removeCliente = (id) => {
     })
 }
 
+const detalhaCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`)
+    .then(resposta => {
+        return resposta.json()
+    })
+}
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type' : 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    .then(resposta => {
+        return resposta.json()
+    })
+}
+
 export const clienteService = {
     listaClientes,
     criaCliente,
-    removeCliente
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
 }
